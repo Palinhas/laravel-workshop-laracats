3ea7901fe9df56dec7da0eccf9b3e09a9afc1869
Carlos Bicho <carlos.s.bicho@example.com>
2026-02-24 17:04:11 +0000

    Implement layout and navigation components; refactor feed and profile pages
    Signed-off-by: Carlos Bicho <carlos.s.bicho@example.com>

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="color-scheme" content="dark" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Pixl</title>
+</head>
+<body class="text-pixl-light flex gap-8 xl:gap-16 px-4 sm:h-dvh sm:overflow-clip">
+ {{ $slot }}
+</body>
+</html>
---
+++ resources/views/partials/aside.blade.php
@@ -0,0 +1,91 @@
+<aside class=" w-80 shrink-0 my-4 hidden lg:block overflow-y-auto px-4 -mx-4">
+    <input
+            type="text"
+            placeholder="#Search an idea or pixel"
+            class="w-full border-pixl/50 text-sm">
+    <hr class="border-pixl/10 mt-2" />
+    <!-- Artists to folow -->
+    <div class="mt-10 border border-pixl-light/10 p-4">
+        <h2 class="text-sm text-pixl-light/60">Artists to follow</h2>
+        <ol class="flex flex-col gap-4 mt-4">
+
+            <li class="flex items-center justify-between gap-4">
+                <div class="flex items-center gap-2.5">
+                    <img src="/images/alessia.png" alt="Avatar of Alessia" class="size-8 object-cover">
+                    <p class="text-sm truncate">alessia_draws</p>
+
+                </div>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-2 py-1 text-sm"
+                >Follow</button
+                >
+            </li>
+            <li class="flex items-center justify-between gap-4">
+                <div class="flex items-center gap-2.5">
+                    <img src="/images/anne.png" alt="Avatar of Anne" class="size-8 object-cover">
+                    <p class="text-sm truncate">jstt_anne</p>
+
+                </div>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-2 py-1 text-sm"
+                >Follow</button
+                >
+            </li>
+            <li class="flex items-center justify-between gap-4">
+                <div class="flex items-center gap-2.5">
+                    <img src="/images/mr-anderson.png" alt="Avatar of Mr. Anderson" class="size-8 object-cover">
+                    <p class="text-sm truncate">Mr. Anderson</p>
+
+                </div>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-2 py-1 text-sm"
+                >Follow</button
+                >
+            </li>
+            <li class="flex items-center justify-between gap-4">
+                <div class="flex items-center gap-2.5">
+                    <img src="/images/michael.png" alt="Avatar of Michael" class="size-8 object-cover">
+                    <p class="text-sm truncate">Michael</p>
+
+                </div>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-2 py-1 text-sm"
+                >Follow</button
+                >
+            </li>
+        </ol>
+        <a href="#" class="text-pixl-light/60 text-sm mt-4 inline-block"
+        >Show More</a
+        >
+    </div>
+    <!-- Follow idea stream -->
+    <div class="mt-4 border border-pixl-light/10 p-4">
+        <h2 class="text-sm text-pixl-light/60">Follow idea Streams</h2>
+        <ol class="flex flex-col gap-4 mt-4">
+            <li class="flex items-center justify-between gap-2.5">
+                <p class="text-sm truncate">Breakfast ideas and whatnot</p>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-1.5 py-0.5 text-2xl leading-none"
+                >+</button
+                >
+            </li>
+            <li class="flex items-center justify-between gap-4">
+                <p class="text-sm truncate">Quick book to read</p>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-1.5 py-0.5 text-2xl leading-none"
+                >+</button
+                >
+            </li>
+            <li class="flex items-center justify-between gap-4">
+                <p class="text-sm truncate">Inspiration for Art</p>
+                <button
+                        class="bg-pixl-dark/50 hover:bg-pixl-dark/60 active:bg-pixl-dark/75 border-pixl/50 active:border-pixl/75 hover:border-pixl/60 text-pixl border px-1.5 py-0.5 text-2xl leading-none"
+                >+</button
+                >
+            </li>
+        </ol>
+        <a href="#" class="text-pixl-light/60 text-sm mt-4 inline-block"
+        >Show More</a
+        >
+    </div>
+</aside>
